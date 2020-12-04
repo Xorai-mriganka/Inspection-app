7693e1b82cac4a0059bec22841567d48b54eddbd
Xorai-Sweta7399 <70868122+Xorai-Sweta7399@users.noreply.github.com>
2020-12-04 20:28:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Mid from './Mid';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Mid />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Mid.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./mid.css";
+import * as ReactBootStrap from 'react-bootstrap';
+const Mid = () => {
+return(
+    <>
+    <h2>Exterior</h2>
+    <ReactBootStrap.Table>
+    <thead>
+    <tr>
+      <th><div class="dropdown"><div className="button1"><button class="dropbtn">G</button></div></div></th>
+      <th><div class="dropdown"><div className="button2"><button class="dropbtn">F</button></div></div></th>
+      <th><div class="dropdown"><div className="button3"><button class="dropbtn">P</button></div></div></th>
+      <th><section className="text">Hood</section></th>
+      <th><div class="dropleft"><button class="dropbtn">NOTE</button></div></th>
+
+    </tr>
+    <tr>
+      <th><div class="dropdown"><div className="button1"><button class="dropbtn">G</button></div></div></th>
+      <th><div class="dropdown"><div className="button2"><button class="dropbtn">F</button></div></div></th>
+      <th><div class="dropdown"><div className="button3"><button class="dropbtn">P</button></div></div></th>
+      <th><section className="text">Front</section></th>
+      <th><div class="dropleft"><button class="dropbtn">NOTE</button></div></th>
+
+    </tr>
+    <tr>
+      <th><div class="dropdown"><div className="button1"><button class="dropbtn">G</button></div></div></th>
+      <th><div class="dropdown"><div className="button2"><button class="dropbtn">F</button></div></div></th>
+      <th><div class="dropdown"><div className="button3"><button class="dropbtn">P</button></div></div></th>
+      <th><section className="text">Front Bumper</section></th>
+      <th><div class="dropleft"><button class="dropbtn">NOTE</button></div></th>
+
+    </tr>
+    <tr>
+      <th><div class="dropdown"><div className="button1"><button class="dropbtn">G</button></div></div></th>
+      <th><div class="dropdown"><div className="button2"><button class="dropbtn">F</button></div></div></th>
+      <th><div class="dropdown"><div className="button3"><button class="dropbtn">P</button></div></div></th>
+      <th><section className="text">Fenders</section></th>
+      <th><div class="dropleft"><button class="dropbtn">NOTE</button></div></th>
+
+    </tr>
+    <tr>
+      <th><div class="dropdown"><div className="button1"><button class="dropbtn">G</button></div></div></th>
+      <th><div class="dropdown"><div className="button2"><button class="dropbtn">F</button></div></div></th>
+      <th><div class="dropdown"><div className="button3"><button class="dropbtn">P</button></div></div></th>
+      <th><section className="text">Doors</section></th>
+      <th><div class="dropleft"><button class="dropbtn">NOTE</button></div></th>
+
+    </tr>
+    <tr>
+      <th><div class="dropdown"><div className="button1"><button class="dropbtn">G</button></div></div></th>
+      <th><div class="dropdown"><div className="button2"><button class="dropbtn">F</button></div></div></th>
+      <th><div class="dropdown"><div className="button3"><button class="dropbtn">P</button></div></div></th>
+      <th><section className="text">Roof</section></th>
+      <th><div class="dropleft"><button class="dropbtn">NOTE</button></div></th>
+
+    </tr>
+    <tr>
+      <th><div class="dropdown"><div className="button1"><button class="dropbtn">G</button></div></div></th>
+      <th><div class="dropdown"><div className="button2"><button class="dropbtn">F</button></div></div></th>
+      <th><div class="dropdown"><div className="button3"><button class="dropbtn">P</button></div></div></th>
+      <th><section className="text">Rear</section></th>
+      <th><div class="dropleft"><button class="dropbtn">NOTE</button></div></th>
+
+    </tr>
+    <tr>
+      <th><div class="dropdown"><div className="button1"><button class="dropbtn">G</button></div></div></th>
+      <th><div class="dropdown"><div className="button2"><button class="dropbtn">F</button></div></div></th>
+      <th><div class="dropdown"><div className="button3"><button class="dropbtn">P</button></div></div></th>
+      <th><section className="text">Rear Bumper</section></th>
+      <th><div class="dropleft"><button class="dropbtn">NOTE</button></div></th>
+
+    </tr>
+    <tr>
+      <th><div class="dropdown"><div className="button1"><button class="dropbtn">G</button></div></div></th>
+      <th><div class="dropdown"><div className="button2"><button class="dropbtn">F</button></div></div></th>
+      <th><div class="dropdown"><div className="button3"><button class="dropbtn">P</button></div></div></th>
+      <th><section className="text">Trunk</section></th>
+      <th><div class="dropleft"><button class="dropbtn">NOTE</button></div></th>
+
+    </tr>
+    <tr>
+      <th><div class="dropdown"><div className="button1"><button class="dropbtn">G</button></div></div></th>
+      <th><div class="dropdown"><div className="button2"><button class="dropbtn">F</button></div></div></th>
+      <th><div class="dropdown"><div className="button3"><button class="dropbtn">P</button></div></div></th>
+      <th><section className="text">Trim</section></th>
+      <th><div class="dropleft"><button class="dropbtn">NOTE</button></div></th>
+
+    </tr>
+  </thead>
+
+</ReactBootStrap.Table>
+</>
+    )
+}
+export default Mid;
